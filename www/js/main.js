2789b4866e72932c4310075663674b8d4e5c6666
Slava <SlavarenikFlance@gmail.com>
2019-12-19 18:42:36 +0200

    fist

---
+++ www/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    let count = $('.count input[type=number]').val()
+
+    $('.show-all .btn').on('click', function(e){
+        e.preventDefault();
+        $('.hidden-list').css('display', 'flex').show()
+    })
+
+    $('.item .up').on('click', function(){
+        changeInputVal($(this), 'increment')
+    })
+
+    $('.item .down').on('click', function(){
+        changeInputVal($(this), 'decrement')
+    })
+
+    function changeInputVal(el, action){
+        if(action === 'increment'){
+            count < 99 ? count++ : count
+        } else if (action === 'decrement'){
+            count > 1 ? count-- : count
+        }
+
+        el.parent().find('input[type=number]').val(count)
+    }
+})
