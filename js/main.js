9225473cb176ad56983b818bc1c60a51c0b7b7b6
Slava <SlavarenikFlance@gmail.com>
2019-12-22 21:11:50 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,21 +1,6 @@
-$(document).ready(function(){
-    let count = 1
-
-    $('.item .up').on('click', function(){
-        changeInputVal($(this), 'increment')
+$(document).ready(() => {
+    $('header .toggle').on('click', function(){
+        $(this).toggleClass('close')
+        $('header').toggleClass('active')
     })
-
-    $('.item .down').on('click', function(){
-        changeInputVal($(this), 'decrement')
-    })
-
-    function changeInputVal(el, action){
-        if(action === 'increment'){
-            count < 99 ? count++ : count
-        } else if (action === 'decrement'){
-            count > 1 ? count-- : count
-        }
-
-        el.parent().find('input[type=number]').val(count)
-    }
 })
