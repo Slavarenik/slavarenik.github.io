f6e3717f1da357dc13ed538ad04e47ad121ec4c8
Slava <SlavarenikFlance@gmail.com>
2019-12-22 21:56:46 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 $(document).ready(() => {
-    var vid = document.getElementById("video");
-
     $('header .toggle').on('click', function(){
         $(this).toggleClass('close')
         $('header').toggleClass('active')
     })
-
-    vid.play()
 })
